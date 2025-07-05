a3aac0e5baf71fcb3abe8654ddb1f73a5af6075f
dajkagabi <dajkagabi01@gmail.com>
2025-07-05 13:11:17 +0200

    hero changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,41 @@
+import video from '../../assets/ll.mp4';
+import {Link} from 'react-router-dom';
 import React from "react";
-import { Link } from "react-router-dom";
-import heroBg from "../../assets/pf.jpg"; 
 
 const Hero = () => {
   return (
-    <section
-      className="relative h-[100vh] bg-cover bg-center flex items-center justify-center text-white"
-      style={{ backgroundImage: `url(${heroBg})` }}
-    >
-      
+    <section className="relative w-full h-[100dvh] overflow-hidden">
+      {/* Háttérvideó */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+      >
+        <source src={video} type="video/mp4" />
      
+      </video>
 
-      <div className="relative z-10 text-center px-4">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4 text-orange-300 drop-shadow-xl">
-          Signature Scent
+      {/* Overlay */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-10" />
+
+      {/* Szöveg tartalom */}
+      <div className="relative z-20 flex flex-col justify-center items-center text-center text-white h-full px-4">
+        <h1 className="text-3xl md:text-5xl font-bold drop-shadow-lg mb-2">
+        Luxurious fragrances crafted with the finest ingredients to define your unique presence.
         </h1>
-        <p className="text-lg md:text-xl max-w-xl mx-auto mb-6 drop-shadow">
-          Luxurious fragrances crafted with the finest ingredients to define your unique presence.
+        <p className="text-base md:text-xl drop-shadow-md mb-4">
+          Explore Scents
         </p>
-
-        <div className="flex flex-col sm:flex-row gap-4 justify-center">
+       <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <Link
-            to="/collections"
-            className="px-6 py-3 bg-black text-white font-medium rounded-full hover:bg-gray-800 transition"
+            to="/category"
+            className="px-6 py-3 bg-amber-100 text-black font-medium rounded-full"
           >
             Shop Collection
           </Link>
-          <Link
-            to="/scents"
-            className="px-6 py-3 bg-white text-black font-medium rounded-full hover:bg-gray-200 transition"
-          >
-            Explore Scents
-          </Link>
-        </div>
+      </div>
       </div>
     </section>
   );
